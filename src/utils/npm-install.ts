8f6950037091a3729a93c64062f2a0883a8e1d3b
Drew Immerman <30954849+DrewImm@users.noreply.github.com>
2023-10-12 11:54:22 -0400

    [Issue #6] Create command fails during npm install (#7)

--- src/utils/npm-install.ts
+++ src/utils/npm-install.ts
@@ -6,7 +6,45 @@ import { CommandRunner } from 'ts-script';
  * @param runner Command runner
  */
 export function npmInstall(runner: CommandRunner): void {
-	runner.run('npm i && npm update && npm audit fix', {
-		loadingDescription: 'Installing npm dependencies',
-	});
+	try {
+		runner.run('npm i', {
+			loadingDescription: 'Installing npm dependencies',
+		});
+	}
+	catch (e) {
+		runner.log.error(
+			'Unable to install npm dependencies. ' +
+				'Please check your connection and manually run `npm i`.'
+		);
+
+		return;
+	}
+
+	try {
+		runner.run('npm update', {
+			loadingDescription: 'Updating npm dependencies',
+		});
+	}
+	catch (e) {
+		runner.log.error(
+			'Unable to update npm dependencies. ' +
+				'Please check your connection and manually run `npm update`.'
+		);
+
+		return;
+	}
+
+	try {
+		runner.run('npm audit fix', {
+			loadingDescription: 'Auditing npm dependencies',
+		});
+	}
+	catch (e) {
+		runner.log.error(
+			'Unable to automatically audit npm dependencies. ' +
+				'Please check your connection and run `npm audit fix`.'
+		);
+
+		return;
+	}
 }
